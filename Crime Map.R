8be29a9d1973eb63a7f7291381777edd2638a165
Lena Nguyen <lena.nguyen4@gmail.com>
2016-03-13 16:21:39 -0400

    Added simple bar graph for district with most crime

--- Crime Map.R
+++ Crime Map.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 library(dplyr)
 library(ggmap)
 library(ggplot2)
@@ -6,18 +8,44 @@ library(readr)
 setwd("/Users/Zelda/Data Science/SF_crime")
 
 # Load dataset
-train <- read.csv("/train.csv")
+crime <- read.csv("train.csv", header=TRUE)
 
 # Quick look at the data
-str(train)
-head(train, n=10)
+str(crime)
+head(crime, n=10)
+
+# Convert dates variable into POSIXct for more date/time functionality
+crime$Dates <- strptime(crime$Dates, format="%Y-%m-%d %H:%M:%S")
+class(crime$Dates)  # Check it worked
+
+# Split date into separate variables
+crime$day <- crime$Dates$mday  # Date
+crime$month <- crime$Dates$mon + 1  # Month
+crime$hour <- crime$Dates$hour  # Hour of the day
+
+head(crime)  # Check it worked
+
+###--- DATA VISUALIZATION ---###
+
+# Which neighborhood has the most crime?
+
+
+plot1<- ggplot(data=crime,
+               aes(x=reorder(PdDistrict, -table(PdDistrict)[PdDistrict]))) +
+        geom_bar(stat="count", width=0.5, fill="steelblue") +
+        scale_size_area() +
+        xlab("District") +
+        ylab("Count") +
+        theme(axis.text.x = element_text(angle = 45, vjust=1, hjust = 1))
+
+###--- MAPPING OF SF ---###
 
 # Get map of SF
 map< qmap(location="sanfrancisco",zoom=12,source="osm",color="bw")
 
 counts<- summarise(group_by(counts, Category), Counts=length(Category))
 counts<- counts[order(-counts$Counts)]
-top12 <- train[train$Category %in% counts$Category[c(1,3:13)],]
+top12 <- crime[crime$Category %in% counts$Category[c(1,3:13)],]
 
 # Generate map
 p<- ggmap(map) +
