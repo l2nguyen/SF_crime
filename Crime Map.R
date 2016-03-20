88008b4ac7dcad84dc31383a481bd3c8d73af4d4
Lena Nguyen <lena.nguyen4@gmail.com>
2016-03-20 17:06:32 -0400

    Made data frame with top 10 crimes. Added a plot to see if crime occurs any more on any day of the week.

--- Crime Map.R
+++ Crime Map.R
@@ -14,6 +14,11 @@ crime <- read.csv("train.csv", header=TRUE)
 str(crime)
 head(crime, n=10)
 
+counts<- crime %>% count(Category, sort=TRUE)
+# Make data frame with only top 10 crimes
+top10 <- crime %>%
+          filter(Category %in% counts$Category[1:10])
+
 # Convert dates variable into POSIXct for more date/time functionality
 crime$Dates <- strptime(crime$Dates, format="%Y-%m-%d %H:%M:%S")
 class(crime$Dates)  # Check it worked
@@ -41,11 +46,6 @@ unique(crime$Category)
 
 ###--- DATA VISUALIZATION ---###
 
-# Hours where the most crime occurs
-ggplot(data=mapdata, aes(x=Hour)) +
-  geom_bar(colour="black", fill="skyblue") +
-  ylab('Count')
-
 # Which neighborhood has the most crime?
 plot1<- ggplot(data=crime,
                aes(x=reorder(PdDistrict, -table(PdDistrict)[PdDistrict]))) +
@@ -55,18 +55,28 @@ plot1<- ggplot(data=crime,
         ylab("Count") +
         theme(axis.text.x = element_text(angle = 45, vjust=1, hjust = 1))
 
+# HOURS where the most crime occurs
+plot2<-   ggplot(data=crime, aes(x=Hour)) +
+          geom_bar(colour="black", fill="royalblue") +
+          xlab("Hour of Day") +
+          ylab('Count')
+# Fewer crime occur around 1-6 AM
+
+# DAYS OF THE WEEK where the most crime occurs
+plot3<-   ggplot(data=crime, aes(x=DayOfWeek)) +
+  geom_bar(colour="black", fill="royalblue") +
+  xlab("Day of the Week") +
+  ylab('Count')
+# Crime seems to be happening every day
+
 ###--- MAPPING OF SF ---###
 
 # Get map of SF
-map< qmap(location="sanfrancisco",zoom=12,source="osm",color="bw")
-
-counts<- summarise(group_by(counts, Category), Counts=length(Category))
-counts<- counts[order(-counts$Counts)]
-top12 <- crime[crime$Category %in% counts$Category[c(1,3:13)],]
+map<- get_map(location="sanfrancisco",zoom=12,source="osm",color="bw")
 
 # Generate map
 p<- ggmap(map) +
-    geom_point(data=top12, aes(x=X, y=Y, color=factor(Category)), alpha=0.1) +
+    geom_point(data=top10, aes(x=X, y=Y, color=factor(Category)), alpha=0.1) +
     guides(colour = guide_legend(override.aes = list(alpha=1.0, size=6.0),
                                  title="Type of Crime")) +
     scale_colour_brewer(type="qual",palette="Paired") +
